8617a1ab4de8a5e187ddc30f1f316ee191f0b050
Martin Kašpar <hello@martinkaspar.net>
2018-08-20 20:52:08 +0200

    Current weather for GTM

--- src/currentweather.js
+++ src/currentweather.js
@@ -19,28 +19,32 @@ var expire = 60; // Time to store weather information in minutes
 		xhr.send();
 	}
 	
-	function pushWeather(station, weather, temp) {
+	function pushWeather(st, gr, ds, tp) {
 		window.dataLayer = window.dataLayer || [];
 		dataLayer.push({
 			event: 'weather',
-			station: station,
-			weather: weather,
-			temperature: temp
+			weather:{
+				station: st,
+				main: gr,
+				description: ds,
+				temperature: tp
+			}
 		});
 	}
 	
 	function setWeather(data) {
 		var station = data.list[0].name;
-		var weather = data.list[0].weather[0].description;
-		var temp = Math.round(data.list[0].main.temp);
+		var main    = data.list[0].weather[0].main;
+		var detail  = data.list[0].weather[0].description;
+		var temp    = Math.round(data.list[0].main.temp);
 		
-		pushWeather(station, weather, temp);
+		pushWeather(station, main, detail, temp);
 		
-		var date = new Date();
-		date.setTime(date.getTime() + (expire * 60000));
-		document.cookie = 'weather_station=' + station + '; expires=' + date.toGMTString() + '; path=/';
-		document.cookie = 'weather_current=' + weather + '; expires=' + date.toGMTString() + '; path=/';
-		document.cookie = 'weather_temperature=' + temp + '; expires=' + date.toGMTString() + '; path=/';
+		var date = new Date(); date.setTime(date.getTime() + (expire * 60000));
+		document.cookie = 'gtm.weather.station=' + station + '; expires=' + date.toGMTString() + '; path=/';
+		document.cookie = 'gtm.weather.main=' + main + '; expires=' + date.toGMTString() + '; path=/';
+		document.cookie = 'gtm.weather.detail=' + detail + '; expires=' + date.toGMTString() + '; path=/';
+		document.cookie = 'gtm.weather.temperature=' + temp + '; expires=' + date.toGMTString() + '; path=/';
 	}
 	
 	function getWeather(data) {
@@ -55,12 +59,13 @@ var expire = 60; // Time to store weather information in minutes
 	}
 	
 	if (navigator.cookieEnabled) {
-		var station = (document.cookie.match(/^(?:.*;)?\s*weather_station=\s*([^;]+)(?:.*)?$/)||[,null])[1];
-		var weather = (document.cookie.match(/^(?:.*;)?\s*weather_current=\s*([^;]+)(?:.*)?$/)||[,null])[1];
-		var temp    = (document.cookie.match(/^(?:.*;)?\s*weather_temperature=\s*([^;]+)(?:.*)?$/)||[,null])[1];
+		var station = (document.cookie.match(/^(?:.*;)?\s*gtm.weather.station=\s*([^;]+)(?:.*)?$/)||[,null])[1];
+		var main    = (document.cookie.match(/^(?:.*;)?\s*gtm.weather.main=\s*([^;]+)(?:.*)?$/)||[,null])[1];
+		var detail  = (document.cookie.match(/^(?:.*;)?\s*gtm.weather.detail=\s*([^;]+)(?:.*)?$/)||[,null])[1];
+		var temp    = (document.cookie.match(/^(?:.*;)?\s*gtm.weather.temperature=\s*([^;]+)(?:.*)?$/)||[,null])[1];
 		
-		if (weather !== null && temp !== null && station !== null) {
-			pushWeather(station, weather, temp);
+		if (station !== null && main !== null && detail !== null && temp !== null) {
+			pushWeather(station, main, detail, temp);
 		} else {
 			currentWeather();
 		}
